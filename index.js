86ad89a56ccdfdb3fd190fe9a43756cb1ef89b9b
andre shen <andregomars@gmail.com>
2021-10-10 10:49:08 -0700

    set up essential local env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 // Please use apollo server to implement your graphql query
@@ -13,6 +15,9 @@ app.get('*', (req, res, next) => {
     res.send("Good luck! 😀")
 });
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: 4000 }, () => {
+    console.log(`ENV`, process.env.ENV);
+    console.log(`AUTH_TOKEN_SIMPLERETS`, process.env.AUTH_TOKEN_SIMPLERETS);
+    console.log(`AUTH_TOKEN_GRAPHQL`, process.env.AUTH_TOKEN_GRAPHQL);
     console.log(`Listening on http://localhost:4000/graphql`)
-);
+});
